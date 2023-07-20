85ca3633993388d26ebdd96da27116f4aaf08758
MrPiccolo23 <sattar1994@hotmail.com>
2023-07-20 14:58:22 +0100

    Made code corrections to App.js and navbar.css

--- src/App.js
+++ src/App.js
@@ -5,59 +5,54 @@ import { Cat } from './components/Cat';
 import Navbar from './components/navBar';
 
 const App = () => {
-	//set state, pass them down as props
-	const [cat, setCat] = useState([]);
-	const [catInfo, setCatInfo] = useState([]);
-	const [basketItems, setBasketItems] = useState([]);
+    const [cat, setCat] = useState([]);
+    const [catInfo, setCatInfo] = useState([]);
+    const [basketItems, setBasketItems] = useState([]);
 
-	//add cat to basket
-	const addToBasket = (item) => {
-		setBasketItems([...basketItems, item]);
-	};
-	//remove cat from basket funtion using remaining cat id
-	const removeItemFromBasket = (item) => {
-		const remainingBasketItems = basketItems.filter(
-			(cat) => cat.id !== item.id,
-		);
-		setBasketItems(remainingBasketItems);
-	};
+    const addToBasket = (item) => {
+        setBasketItems([...basketItems, item]);
+    };
 
-	//fetch cat info from cat api and faker
-	useEffect(() => {
-		(async () => {
-			const res = await fetch('https://api.thecatapi.com/v1/breeds?limit=20');
-			const data = await res.json();
-			const info = fetchCatData();
+    const removeItemFromBasket = (item) => {
+        const remainingBasketItems = basketItems.filter(
+            (cat) => cat.id !== item.id,
+        );
+        setBasketItems(remainingBasketItems);
+    };
 
-			setCatInfo(info);
-			setCat(data);
-		})();
-	}, []);
+    useEffect(() => {
+        (async () => {
+            const res = await fetch('https://api.thecatapi.com/v1/breeds?limit=20');
+            const data = await res.json();
+            const info = fetchCatData();
 
-	return (
-		//pass functions and state down as props to components
-		//map method populates a new array
-		<div>
-			<Navbar
-				basketItems={basketItems}
-				removeItemFromBasket={removeItemFromBasket}
-			/>
-			<div className="catlist">
-				{cat.map((cat, i) => (
-					<Cat
-						key={cat.id}
-						id={cat.id}
-						catName={catInfo[i]?.name}
-						image={cat.image.url}
-						name={cat.name}
-						temperament={cat.temperament}
-						price={catInfo[i].price}
-						addToBasket={addToBasket}
-					/>
-				))}
-			</div>
-		</div>
-	);
+            setCatInfo(info);
+            setCat(data);
+        })();
+    }, []);
+
+    return (
+        <div>
+            <Navbar
+                basketItems={basketItems}
+                removeItemFromBasket={removeItemFromBasket}
+            />
+            <div className="catlist">
+                {cat.map((cat, i) => (
+                    <Cat
+                        key={cat.id}
+                        id={cat.id}
+                        catName={catInfo[i]?.name}
+                        image={cat.image?.url} // Make sure cat.image is defined before accessing its url property
+                        name={cat.name}
+                        temperament={cat.temperament}
+                        price={catInfo[i]?.price} // Make sure catInfo[i] is defined before accessing its price property
+                        addToBasket={addToBasket}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default App;
